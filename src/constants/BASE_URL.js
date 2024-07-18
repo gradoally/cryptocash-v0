76743bc13bd9eb5a444c599f9cca3630c02b1ed7
Alex Breton <161599311+printdreams@users.noreply.github.com>
2024-07-18 23:33:16 +0200

    Update BASE_URL.js
    Pointing to backend in Heroku:
    https://cryptocashbackend-f55cc665d1b6.herokuapp.com/

--- src/constants/BASE_URL.js
+++ src/constants/BASE_URL.js
@@ -1,3 +1,4 @@
-const BASE_URL = 'https://vegaolga5938.store'
+const BASE_URL = 'https://cryptocashbackend-f55cc665d1b6.herokuapp.com/'
+//https://vegaolga5938.store
 //http://127.0.0.1:5000
 export default BASE_URL
